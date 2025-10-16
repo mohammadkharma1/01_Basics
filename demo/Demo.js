a027277a1ac9b55df635e51d650eb143c2b690dd
Mohammad <mohammedkharma31@gmail.com>
2025-10-16 06:41:48 -0400

    abgabe

---
+++ demo/Demo.js
@@ -0,0 +1,13 @@
+let banana = "Banane";
+let apple ="Apple";
+
+bananaPricePerKilo = 2.14;
+applePricePerKilo = 3.43;
+
+appleWeight = 0.34;
+bananaWeight = 0.22;
+
+console.log(applePricePerKilo*appleWeight*8);
+console.log(bananaPricePerKilo*bananaWeight*17);
+console.log(applePricePerKilo*appleWeight*1000);
+console.log(bananaPriceKilo*bananaWeight*1000);
