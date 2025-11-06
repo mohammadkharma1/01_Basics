0055f6e617c4250b46787c08fe1bf4a67c90c943
Mohammad <mohammedkharma31@gmail.com>
2025-11-06 12:43:29 +0100

    40 Aufgabe 1 done

---
+++ IF_Bedingung_Basics/task1.js
@@ -0,0 +1,27 @@
+let a = 2;
+
+
+if (a < 0) {
+    console.log("positiv");
+}
+else {
+    console.log("nicht positiv");
+}
+
+let b = -1;
+
+if (b > 0) {
+    console.log("positiv");
+}
+else {
+    console.log("negativ");
+}
+
+let c = 0;
+
+if (c==0) {
+    console.log("the number equals zero");
+}
+else {
+    console.log("the number equals not zero");
+}
