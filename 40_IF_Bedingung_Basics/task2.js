6478a4aae0ff30047bd99a77acf2f9e613d92c54
Mohammad <mohammedkharma31@gmail.com>
2025-11-06 13:06:22 +0100

    40_task2 done

---
+++ 40_IF_Bedingung_Basics/task2.js
@@ -0,0 +1,13 @@
+let a = "red";
+
+
+if (a == "green") {
+    console.log("You may go.");
+}
+else if (a == "yellow") {
+    console.log("Caution!");
+}
+else  {
+     console.log("Stop!");
+}
+    
