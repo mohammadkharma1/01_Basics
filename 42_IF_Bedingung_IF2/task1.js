e26891b35d12534444bd3790511741812bac052c
Mohammad <mohammedkharma31@gmail.com>
2025-11-13 12:48:50 +0100

    task1.js 42_IF_Bedingung_IF2 Finished

---
+++ 42_IF_Bedingung_IF2/task1.js
@@ -0,0 +1,14 @@
+let randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
+
+
+if (randomNumber < 5){
+    console.log("the number is less than 5");
+}
+else if (randomNumber > 5){
+    console.log("the number is greater than 5");
+}
+else if (randomNumber == 5){
+    console.log("the number is exactly 5");
+}
+    
