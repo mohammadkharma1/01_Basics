2c3f27673b05a92cf8572b5522beadde1a7b16d2
Mohammad <mohammedkharma31@gmail.com>
2025-11-17 11:15:57 +0100

    Deno task1 and task 2 finish cancellation

---
+++ 45_Deno/task1.js
@@ -0,0 +1,9 @@
+let password =("1000");
+let input = prompt ("Enter your password");
+
+if (input === password) {
+    console.log("Access granted");
+}
+if (input !== password){
+    console.log("Access denied");
+}
---
+++ 45_Deno/task2.js
@@ -0,0 +1,18 @@
+let a = (10);
+
+if (a>0) {
+    console.log("The number is positive");
+}
+else {
+    console.log("the number is not positive");
+}
+if (a < 0){
+    console.log("the number is negative");
+} else {
+    console.log("the number is not negative");
+} 
+if (a == 0) {
+    console.log("the number is zero")
+} else {
+    console.log("the number is not 0");
+}
