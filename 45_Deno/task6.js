6bcf5e3d4a5ba5ff1066860a6d3bacf6b101870f
Mohammad <mohammedkharma31@gmail.com>
2025-12-04 13:04:26 +0100

    task 6 finish 45 _Deno

---
+++ 45_Deno/task6.js
@@ -0,0 +1,11 @@
+let a = prompt("Welches Getrank hättest du gerne?");
+
+if (a === "Cola") {
+  console.log("Danke für deine Bestellung von Cola");
+} else if (a === "Wasser") {
+  console.log("Danke für deine Bestellung von Wasser.");
+} else if (a === "Tee") {
+  console.log("Danke für deine Bestellung von Tee.");
+} else if (a === "Bier") {
+  console.log("Danke für deine Bestellung von Bier.");
+}
