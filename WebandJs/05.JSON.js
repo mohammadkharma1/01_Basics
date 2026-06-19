30779b26cbdaf5f2c019e8c5898f0fe8f77b5f5c
Mohammad <mohammedkharma31@gmail.com>
2026-06-19 09:44:42 +0200

    5 Video fertig

---
+++ WebandJs/05.JSON.js
@@ -0,0 +1,19 @@
+let data = [
+  { firstName: "Hans", lastName: "Huber", points: 30 },
+  { firstName: "Paul", lastName: "Müller", points: 34 },
+  { firstName: "Franz", lastName: "Maier", points: 35 },
+];
+ 
+for (let index = 0; index < data.length; index++) {
+  const element = data[index];
+  console.log(element.firstName);
+}
+ 
+function loadPeople() {
+  let html = "";
+  (data.forEach((element) => {
+    html += "<div>" + element.firstName + " " + element.lastName + "</div>";
+  }),
+    (document.getElementById("content").innerHTML = html));
+}
+loadPeople();
